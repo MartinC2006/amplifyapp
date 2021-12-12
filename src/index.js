d57deedf3e374237e743af873c7042b07b8833bf
MartinC2006 <mjc2006@hotmail.co.uk>
2021-12-12 20:45:27 +0000

    3rd commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Amplify from 'aws-amplify'
-import awsconfig from './aws-exports'
+//import Amplify from 'aws-amplify'
+//import awsconfig from './aws-exports'
 
-Amplify.configure(awsconfig)
+//Amplify.configure(awsconfig)
 
 
 ReactDOM.render(
